c972faeb129489260342dd37f781ef9b02a0455b
o251298 <o251298@gmail.com>
2022-08-20 17:57:16 +0300

    add migrations 2 tables

--- public/index.php
+++ public/index.php
@@ -1,14 +1,7 @@
 <?php
-echo 12;
-die();
 require_once './../vendor/autoload.php';
 require_once '../routes/web.php';
 use Dotenv\Dotenv;
 use App\Router;
 use App\Models\Transport;
-Dotenv::createMutable(dirname(dirname(__FILE__)))->load();
-//(new Router($routes))->run();
-use App\Services\Collection\Collection;
-use App\Models\Categorie;
-//$collection = new Collection([new Categorie(1,'asd', 'qweq'), new Categorie(2,'asqwed324', 'qwe234q'), new Categorie(3,'qweasd', 'qweasdq')]);
-Categorie::all();
+Dotenv::createMutable(dirname(dirname(__FILE__)))->load();(new Router($routes))->run();
